580841f2800dc8b6433b04c8e1ede424dc4916ab
Dan Vera <veradan1999@gmail.com>
2022-03-17 15:36:49 -0400

    Added comments to update_last_login.php

--- update_last_login.php
+++ update_last_login.php
@@ -1,5 +1,6 @@
 <?php
-$sql_update = "UPDATE 2022S_CPS3961_01.Users SET last_login = current_timestamp() where user_ia = '$user_id'";
+//updates last_login in the Users table
+$sql_update = "UPDATE 2022S_CPS3961_01.Users SET last_login = current_timestamp() where user_id = '$user_id'";
 $result_update = mysqli_query($con, $sql_update);
 
 if(!$result_update) {
