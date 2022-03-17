928a749942f3d6d4b9484654c904538098a5faea
Dan Vera <veradan1999@gmail.com>
2022-03-17 15:37:36 -0400

    Added comments to update_location.php

--- update_location.php
+++ update_location.php
@@ -1,18 +1,9 @@
 <?php
-$ip = $_SERVER['REMOTE_ADDR'];
-//$ip = getRealIpAddr();
-/*
-if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-    $ip = $_SERVER['HTTP_CLIENT_IP'];
-} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-} else {
-    $ip = $_SERVER['REMOTE_ADDR'];
-}
-*/
+//updates user location cookies
+$ip = $_SERVER['REMOTE_ADDR'];  //retrieves user IP
 setcookie("ip", $ip, time() + 60*60);
 
-$geo_url = 'https://freegeoip.app/json/' . $ip;
+$geo_url = 'https://freegeoip.app/json/' . $ip; //retrieves user location inforamtion using their ip
 $geo_file = file_get_contents($geo_url);
 $geo_arr = json_decode($geo_file, true);
 $country = $geo_arr['country_name'];
@@ -29,20 +20,4 @@ $city = $geo_arr['city'];
 setcookie("city", $city, time() + 60*60);
 $state = $geo_arr['region_name'];
 setcookie("state", $state, time() + 60*60);
-/*
-//https://itman.in/en/how-to-get-client-ip-address-in-php/#:~:text=%24_SERVER%5B'REMOTE_ADDR'%5D,this%20value%20is%20easily%20spoofed.
-function getRealIpAddr() {
-    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {  //check ip from share internet 
-      $ip=$_SERVER['HTTP_CLIENT_IP'];
-    }
-    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {   //to check ip is pass from proxy
-      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
-    }
-    else {
-      $ip=$_SERVER['REMOTE_ADDR'];
-    }
-    return $ip;
-}
-*/
-
 ?>
