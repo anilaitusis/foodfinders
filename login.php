5d42739b6a817472b24d9d92866b43a0ae39b3e6
Dan Vera <veradan1999@gmail.com>
2022-03-17 15:34:20 -0400

    Added comments to login.php

--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+//Used to log the user in 
 $email_username=$_POST['email'];
 $cust_password=$_POST['password'];
 
@@ -6,7 +7,7 @@ include("dbconfig.php");
 $con = mysqli_connect($host,$username,$password,$dbname);
 
 $sql = "SELECT user_id, email, username, password, active, last_login FROM 2022S_CPS3961_01.Users where email = '$email_username' or username = '$email_username'";
-$result = mysqli_query($con, $sql); 
+$result = mysqli_query($con, $sql); //retrieves user info from Users table that match correspond with the email/username they entered
 
 if($result) {
     if (mysqli_num_rows($result) > 0) {
@@ -17,21 +18,18 @@ if($result) {
             $c_password = $row['password'];
             $c_active = $row['active'];
             $last_login = $row['last_login'];
-            if($c_password == $cust_password){
-                if($c_active == 0) {
+            if($c_password == $cust_password){  //checks if the password they entered matches the one in the DB
+                if($c_active == 0) {    //checks if user is active, if not requests their activation key
                     echo "<form action='activate_user.php' method='post'>
                         <br>Activation key: <input type='text' name='active_key' required='required'>
                         <input type='hidden' name='user_id' value='$user_id'>
                         <input type='submit' value='Submit'>";
                 }
-                setcookie("user_id",$user_id,time() + 60*60);
+                setcookie("user_id",$user_id,time() + 60*60);   //sets $user_id and $c_username as cookies
                 setcookie("username",$c_username,time() + 60*60);
-                include("update_last_login.php");
-                if(!isset($_COOKIE['ip'])) {
-                    include("update_location.php");
-                }
-                include("update_location.php");            
-                header("Location: http://obi.kean.edu/~veradan/CPS3962/user_home.php");
+                include("update_last_login.php"); //updates the last_login in the User table
+                include("update_location.php"); //updates the user location cookies        
+                header("Location: http://obi.kean.edu/~veradan/CPS3962/user_home.php"); //brings user to user_home.php once logged in, can bring them to an HTML page if better option
                 exit;
          
             }
