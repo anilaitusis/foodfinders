6cef130f1379b92c2d182147525419b252c4cbd7
Dan Vera <veradan1999@gmail.com>
2022-03-31 14:57:39 -0400

    Removed unnecessary cookies

--- login_related/logout.php
+++ login_related/logout.php
@@ -3,17 +3,16 @@
 if (isset($_COOKIE['user_id'])) {
     echo"You have successfully logged out!<br>";
     setcookie("user_id",'',time() - 60);
-    setcookie("ip", $ip, time() - 60);
-    setcookie("country", $country, time() - 60);
-    setcookie("country_code", $country_code, time() - 60);
-    setcookie("zip", $zip, time() - 60);
-    setcookie("lat", $lat, time() - 60);
-    setcookie("lon", $lon, time() - 60);
-    setcookie("city", $city, time() - 60);
-    setcookie("state", $state, time() - 60);
+    setcookie("ip", '', time() - 60);
+    setcookie("country", '', time() - 60);
+    setcookie("zip", '', time() - 60);
+    setcookie("lat", '', time() - 60);
+    setcookie("lon", '', time() - 60);
+    setcookie("city", '', time() - 60);
+    setcookie("state", '', time() - 60);
 }
 else {
     die("Not able to logout");
 }
-echo "<br><a href='index.html'>Return to project homepage</a>"; //brings them back to login page
+echo "<br><a href='index.html'>Return to project homepage</a>";
 ?>
