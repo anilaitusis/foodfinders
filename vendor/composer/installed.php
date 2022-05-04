0c537c7f81dce907fd791f81effe8d426a1ff48b
Tristram Dacayan <dacayant@kean.edu>
2022-05-04 15:11:41 -0400

    composer edits

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '846e398325cf8f428680bc831f4ef03bb6bd1c94',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '846e398325cf8f428680bc831f4ef03bb6bd1c94',
+            'dev_requirement' => false,
+        ),
+    ),
+);
